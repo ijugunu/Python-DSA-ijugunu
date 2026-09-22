1f15d68adc8e276ceed9fbd6fff24353ae3d52b1
Satyam Bhardwaj <bsatyam151298@gmail.com>
2026-09-22 12:13:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minPlatform(self, arr: list[int], dep: list[int]) -> int:
+        n=len(arr)
+        arr.sort()
+        dep.sort()
+        
+        ans=1
+        count=1
+        i=1
+        j=0
+        while i<n:
+            if arr[i]<=dep[j]:
+                count+=1
+                i+=1
+            else:
+                count-=1
+                j+=1
+            ans=max(ans,count)
+        return ans    
+                
